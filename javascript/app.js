6c57c39bc1af683e4df58411066fc7220fe43f68
laith17 <laithalmajaly007@gmail.com>
2023-10-10 20:24:21 +0300

    Added CRUD

--- javascript/app.js
+++ javascript/app.js
@@ -48,3 +48,91 @@ fetch("https://fakestoreapi.com/products?limit=20")
       cardsContainer.appendChild(cardElement);
     });
   });
+
+//* CRUD Section
+
+const titleInput = document.getElementById("title");
+const contentInput = document.getElementById("textarea");
+const createPostButton = document.getElementById("submit");
+const postsList = document.querySelector(".posts");
+
+//* Function to fitch the posts
+function fetchPosts() {
+  fetch("http://localhost:3000/posts")
+    .then((res) => res.json())
+    .then((posts) => {
+      postsList.innerHTML = "";
+      posts.map((post) => {
+        const postBox = document.createElement("div");
+        postBox.classList.add("card");
+        postBox.innerHTML = `
+                <div class="text-box">
+                   <p id="title">${post.title}</p>
+                   <p id="content">${post.content}</p>
+                </div>
+                <div class="buttons">
+                <button class="update" id="${post.id}" onclick="updatePost(this)">Update</button>
+                <button class="delete" id="${post.id}" onclick="deletePost(this)">Delete</button>
+               </div>
+        `;
+        postsList.appendChild(postBox);
+      });
+    });
+}
+
+//* Event Listiner to add a new post
+createPostButton.addEventListener("click", () => {
+  fetch("http://localhost:3000/posts", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: titleInput.value,
+      content: contentInput.value,
+    }),
+  })
+    .then(() => {
+      titleInput = "";
+      contentInput = "";
+      fetchPosts();
+    })
+    .catch((error) => console.log("Error: ", error));
+});
+
+//* Function To Delete A Post
+function deletePost(button) {
+  const postId = button.getAttribute("id");
+  fetch(`http://localhost:3000/posts/${postId}`, {
+    method: "DELETE",
+  })
+    .then(() => {
+      fetchPosts();
+    })
+    .catch((error) => console.log("Error: ", error));
+}
+
+//* Function To Update A Post
+function updatePost(button) {
+  const postId = button.getAttribute("id");
+  const updatedTitle = prompt("Update The Title:", "");
+  const updatedContent = prompt("Update The Content:", "");
+
+  if (updatedTitle !== null || updatedContent !== null) {
+    fetch(`http://localhost:3000/posts/${postId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: updatedTitle,
+        content: updatedContent,
+      }),
+    })
+      .then(() => {
+        fetchPosts();
+      })
+      .catch((error) => console.log("Error: ", error));
+  }
+}
+fetchPosts();
